3423d77171487ba159dade0c3b2a78b4cf334f78
Andrew Kou <Andrew Kou>
2017-09-11 15:22:56 -0700

    update

---
+++ Assets/Scripts/GhostMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMove : MonoBehaviour {
+
+	public Transform[] waypoints; 
+	int current = 0;
+	public GameManager gameManager; 
+	public float speed = 0.3f;
+
+	void FixedUpdate()
+	{
+		if (transform.position != waypoints [current].position) {
+			Vector2 p = Vector2.MoveTowards (transform.position, waypoints [current].position, speed); 
+			GetComponent<Rigidbody2D> ().MovePosition (p);
+		} else {
+			current = (current + 1) % waypoints.Length;
+		}
+
+		Vector2 dir = waypoints [current].position - transform.position;
+		GetComponent<Animator> ().SetFloat ("DirX", dir.y);
+		GetComponent<Animator> ().SetFloat ("DirY", dir.y);
+	}
+
+
+
+	void OnTriggerEnter2D(Collider2D co)
+	{
+		if (co.name == "pacman") {
+			Destroy (co.gameObject); 
+			gameOver();
+			print ("game over"); 
+		}
+	}
+
+	void gameOver(){
+		//Application.LoadLevel (Application.loadedLevel);
+		FindObjectOfType<GameManager>().EndGame();
+		print ("game over"); 
+	}
+}
---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour {
+
+	private GameObject[] dots;
+
+	public float speed = 0.4f;
+	Vector2 dest = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		dest = transform.position;
+		dots = new GameObject[328];
+		GameObject[] obj = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+		var c = 0;
+		for (var i=0; i < obj.Length; i++) {
+			if (obj[i].name.Contains("pacdot"))
+			{
+				dots [c] = obj[i];
+				c++;
+			}
+		}
+
+		if (c == 328)
+			print ("we gucci");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 p = Vector2.MoveTowards (transform.position, dest, speed);
+		GetComponent<Rigidbody2D> ().MovePosition (p);
+
+		if ((Vector2)transform.position == dest) {
+			if (Input.GetKey (KeyCode.UpArrow) && valid(Vector2.up))
+				dest = (Vector2)transform.position + Vector2.up;
+			if (Input.GetKey (KeyCode.RightArrow) && valid(Vector2.right))
+				dest = (Vector2)transform.position + Vector2.right;
+			if (Input.GetKey (KeyCode.DownArrow) && valid(-Vector2.up))
+				dest = (Vector2)transform.position - Vector2.up;
+			if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+				dest = (Vector2)transform.position - Vector2.right;
+		}
+
+		Vector2 dir = dest - (Vector2)transform.position;
+		GetComponent<Animator> ().SetFloat ("DirX", dir.x);
+		GetComponent<Animator> ().SetFloat ("DirY", dir.y);
+	}
+
+	bool valid(Vector2 dir) {
+		Vector2 pos = transform.position;
+		RaycastHit2D hit = Physics2D.Linecast (pos + dir, pos);
+		return (hit.collider == GetComponent<Collider2D>());
+	}
+
+	void OnTriggerEnter2D(Collider2D co)
+	{
+		if (dots.Length == 0) {
+			gameOver ();
+		}
+	}
+
+	void gameOver(){
+		//Application.LoadLevel (Application.loadedLevel);
+		FindObjectOfType<GameManager>().EndGame();
+		print ("game over"); 
+	}
+
+}
